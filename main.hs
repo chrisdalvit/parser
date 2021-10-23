f84b197404b775b9273091892f14b7f6b04e4481
christiandalvit <christian.dalvit@mpreis.at>
2021-10-23 17:08:36 +0200

    eval loop

--- main.hs
+++ main.hs
@@ -3,9 +3,18 @@ import CommandEvaluator (evalCommand)
 import CommandParser ( stringToCommand )
 
 main :: IO()
-main = do
+main = evalLoop []
+
+evalLoop :: [(String, String)] -> IO()
+evalLoop as = do
     inp <- getLine
+    as' <- evalInput inp as
+    evalLoop as'
+
+evalInput :: String -> [(String, String)] -> IO[(String, String)]
+evalInput inp as =
     case stringToCommand inp of
-        Nothing -> putStr ""
-        Just cmd -> evalCommand cmd
-    main
+        Nothing -> return as
+        Just cmd -> do
+            fst $ evalCommand as cmd
+            return $ snd $ evalCommand as cmd
