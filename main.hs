a6694bbb0fa785f66694cbeb7ca51d297ffc279f
christiandalvit <christian.dalvit@mpreis.at>
2021-10-25 18:34:36 +0200

    print values of assignments

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
 import Data.Maybe (mapMaybe)
 import CommandEvaluator (evalCommand)
 import CommandParser ( stringToCommand )
-import Input ( Assignment )
+import Input ( Assignment(..) )
 
 main :: IO()
 main = evalLoop []
@@ -15,5 +15,14 @@ evalLoop as = do
 evalInput :: String -> [Assignment] -> IO[Assignment]
 evalInput inp as =
     case stringToCommand inp of
-        Nothing -> return as
+        Nothing -> do
+            printAssignment inp as
+            return as
         Just cmd -> evalCommand as cmd
+
+
+printAssignment :: String -> [Assignment] -> IO()
+printAssignment s [] = return ()
+printAssignment s (Assignment r l : as)
+    | s == r = print l
+    | otherwise = printAssignment s as
