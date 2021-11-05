9c725ba792a86a5bded9a5420b58aee3a24084fe
christiandalvit <christian.dalvit@mpreis.at>
2021-11-05 09:02:46 +0100

    fix command parsing bug

--- src/Command/CommandParser.hs
+++ src/Command/CommandParser.hs
@@ -1,17 +1,31 @@
 module Command.CommandParser (Command(..), CommandSymbol(..), Args(..), stringToCommand) where
 
-import Utils.Parser (Parser(..), char, spaces, parse, split)
+import Utils.Parser
+import GHC.Unicode (isPrint)
+import Data.Char (isSpace)
+import Control.Applicative ( Alternative((<|>)) )
 
 newtype CommandSymbol = CommandSymbol String deriving Show
 newtype Args = Args [String] deriving Show
 data Command = Command CommandSymbol Args deriving Show
 
 command :: Parser Command
-command = do
+command = commandWithArgs <|> commandWithoutArgs
+
+commandWithArgs :: Parser Command
+commandWithArgs = do
     spaces
     char ':'
     (sym, args) <- split ' '
     return $ Command (CommandSymbol sym) (Args (words args))
+
+commandWithoutArgs :: Parser Command
+commandWithoutArgs = do
+    spaces
+    char ':'
+    sym <- many1 (sat (\c -> isPrint c && not (isSpace c)))
+    return $ Command (CommandSymbol sym) (Args [])
+
 
 stringToCommand :: String -> Maybe Command
 stringToCommand s = case parse command s of
