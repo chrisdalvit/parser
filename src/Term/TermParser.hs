bd99cf1a4b3b34398991ee0746ea691d52228aad
christiandalvit <christian.dalvit@mpreis.at>
2021-11-05 17:51:17 +0100

    fix lpo ordering bug

--- src/Term/TermParser.hs
+++ src/Term/TermParser.hs
@@ -1,4 +1,4 @@
-module Term.TermParser (Term, Rule, stringsToTRS, stringToTerm) where
+module Term.TermParser (Term, Rule, stringsToTRS, stringToTerm, stringToRule) where
 
 import Utils.Parser (Parser, parse, char, string, comma, sep, sat, token, many1)
 import Term.Term ( Rule(..), Term(..), validTRS, validRule)
