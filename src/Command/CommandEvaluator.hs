699f294dd66efd6eb3060182cd59e90704c03495
christiandalvit <christian.dalvit@mpreis.at>
2021-11-05 09:26:18 +0100

    add usage hints

--- src/Command/CommandEvaluator.hs
+++ src/Command/CommandEvaluator.hs
@@ -11,32 +11,43 @@ import Utils.Precedence (argsToPrecedence, Precedence(..))
 import Text.Read (prec)
 import System.Exit (exitSuccess)
 
+commandName :: Command -> String
+commandName (Command (CommandSymbol sym) _) = sym
+
+args :: Command -> Args
+args (Command _ args) = args
+
 evalCommand :: [Assignment] -> Command -> IO [Assignment]
-evalCommand as (Command (CommandSymbol "term") args) = evalTermCommand as args
-evalCommand as (Command (CommandSymbol "trs") args ) = evalTRSCommand as args
-evalCommand as (Command (CommandSymbol "trsfile") args ) = readTRSFile as args
-evalCommand as (Command (CommandSymbol "lpo") args ) = evalLPO as args
-evalCommand as (Command (CommandSymbol "pred") args ) = evalPrecedenceCommand as args
-evalCommand as (Command (CommandSymbol "evalfile") args) = do
-    evalFileCommand args
-    return as
-evalCommand as (Command (CommandSymbol "pd") args) = evalPatternDisjointness as args
-evalCommand as (Command (CommandSymbol "=") args) = evalAssignmentCommand as args
-evalCommand as (Command (CommandSymbol "q") args) = exitSuccess
-evalCommand as _ = do
-    putStrLn " -- Non-valid command -- "
-    return as
+evalCommand as cmd
+    | name == "term" = evalTermCommand as cmdArgs
+    | name == "trs" = evalTRSCommand as cmdArgs
+    | name == "trsfile" = readTRSFile as cmdArgs
+    | name == "lpo" = evalLPO as cmdArgs
+    | name == "pred" = evalPrecedenceCommand as cmdArgs
+    | name == "pd" = evalPatternDisjointness as cmdArgs
+    | name == "evalfile" = do
+        evalFileCommand cmdArgs
+        return as
+    | name == "q" = exitSuccess
+    | otherwise = do
+        putStrLn " -- Non-valid command -- "
+        return as
+    where
+        name = commandName cmd
+        cmdArgs = args cmd
 
 evalPrecedenceCommand :: [Assignment] -> Args -> IO [Assignment]
-evalPrecedenceCommand as (Args (n:a:args)) = do 
+evalPrecedenceCommand as (Args (n:a:args)) = do
     case argsToPrecedence (a:args) of
       Nothing -> do
           putStrLn " -- Error: parsing precedence -- ″"
           return as
-      Just prece -> do 
+      Just prece -> do
         return $ addAssignment (Assignment n (Precedence prece)) as
-evalPrecedenceCommand as _ = do 
-    putStrLn " -- Error: wrong number of arguments -- "
+evalPrecedenceCommand as _ = do
+    putStrLn " -- Usage -- "
+    putStrLn " :pred  PRED_NAME FSYM>FSYM FSYM>FSYM ..."
+    putStrLn " Attention: User is responsible, that entered precedence is valid! \n"
     return as
 
 evalLPO :: [Assignment] -> Args -> IO [Assignment]
@@ -44,70 +55,61 @@ evalLPO as (Args [trs, pred]) = do
     case findAssignment trs as of
         Nothing -> putStrLn $ " -- Error: no trs for " ++ trs ++ " -- ″"
         Just (TRS trs) -> case findAssignment pred as of
-            Nothing -> putStrLn $ " -- Error: no precedence"
+            Nothing -> putStrLn $ " -- Error: no precedence for " ++ pred ++ " -- ″"
             Just (Precedence p) -> print $ isLPOTerminating trs p
             Just _ -> putStrLn " -- Error: provide precedence -- "
         Just _ -> putStrLn " -- Error: provide trs -- "
     return as
 evalLPO as _ = do
-    putStrLn " -- Error: wrong number of arguments -- "
+    putStrLn " -- Usage -- "
+    putStrLn " :lpo  TRS_NAME PRED_NAME \n"
     return as
 
 evalPatternDisjointness :: [Assignment] -> Args -> IO [Assignment]
 evalPatternDisjointness as (Args [x]) = do
     case findAssignment x as of
-        Nothing -> putStrLn $ " -- Error: no value for " ++ x ++ " -- ″"
-        Just (TRS trs) -> print $ isPatternDisjoint trs 
+        Nothing -> putStrLn $ " -- Error: no trs for " ++ x ++ " -- ″"
+        Just (TRS trs) -> print $ isPatternDisjoint trs
         Just _ -> putStrLn " -- Error: provide trs -- "
     return as
 evalPatternDisjointness as _  = do
-    putStrLn " -- Error: wrong number of arguments -- "
+    putStrLn " -- Usage -- "
+    putStrLn " :pd  TRS_NAME \n"
     return as
 
 readTRSFile :: [Assignment] -> Args -> IO [Assignment]
-readTRSFile as (Args [f]) = do
-    file <- readFile f
-    print $ stringsToTRS $ lines file
-    return as
 readTRSFile as (Args [f, n]) = do
     file <- readFile f
     case stringsToTRS $ lines file of
         Nothing -> do
-            putStrLn " -- Error: incorrect TRS -- "
-            return as 
+            putStrLn " -- Error: incorrect TRS file -- "
+            return as
         Just trs -> do
             return $ addAssignment (Assignment n (TRS trs)) as
 readTRSFile as _ = do
-    putStrLn " -- Error: wrong number of arguments -- "
-    return as
-
-evalAssignmentCommand :: [Assignment] -> Args -> IO [Assignment]
-evalAssignmentCommand as (Args [x]) = do
-    case stringToAssignment x of
-        Nothing -> do
-            putStrLn " -- Error when parsing assignment -- "
-            return as
-        Just a -> do 
-            print a
-            return $ addAssignment a as
-evalAssignmentCommand as _ = do 
-    putStrLn " -- Wrong number of arguments -- "
-    return as
+        putStrLn " -- Usage -- "
+        putStrLn " :trsfile  FILE_NAME TRS_NAME \n"
+        return as
 
 evalTermCommand :: [Assignment] -> Args -> IO [Assignment]
-evalTermCommand as (Args [x]) = do
-    print $ stringToTerm x
-    return as
+evalTermCommand as (Args [t]) = case stringToTerm t of
+    Nothing -> do
+        putStrLn " -- No valid term -- "
+        return as
+    Just t -> do
+        print t
+        return as
 evalTermCommand as (Args [n, t]) = case stringToTerm t of
-    Nothing -> do 
+    Nothing -> do
         putStrLn " -- No valid term -- "
         return as
     Just t -> do
         print t
         return $ addAssignment (Assignment n (Term t)) as
-evalTermCommand as _ = do 
-    putStrLn " -- Error: not enough arguments -- "
-    return as
+evalTermCommand as _ = do
+        putStrLn " -- Usage -- "
+        putStrLn " :term TERM \n"
+        return as
 
 evalTRSCommand :: [Assignment] -> Args -> IO [Assignment]
 evalTRSCommand as (Args [n]) = do
@@ -117,16 +119,19 @@ evalTRSCommand as (Args [n]) = do
         _ -> putStrLn $ "TRS: " ++ show trs
     return $ addAssignment (Assignment n (TRS trs)) as
 evalTRSCommand as _ = do
-    putStrLn " -- Error: wrong number of arguments -- "
-    return as
+        putStrLn " -- Usage -- "
+        putStrLn " :trs RULE1 >>ENTER<< RULE2 ... \n"
+        return as
 
 evalFileCommand :: Args -> IO()
 evalFileCommand (Args [fn]) = do
     file <- readFile fn
     case evalFile (splitOn "" (lines file)) of
         Nothing -> putStrLn " -- Error in file evaluation -- "
-        Just res -> putStrLn $ "Result: " ++ show res
-evalFileCommand _ = putStrLn " -- Error: too many or to few arguments -- "
+        Just res -> print res
+evalFileCommand _ = do
+    putStrLn " -- Usage -- "
+    putStrLn " :evalfile FILE_NAME\n"
 
 evalFile :: [[String]] -> Maybe Term
 evalFile [rs, [t]] = do
